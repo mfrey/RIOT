ea3c17cf333f1fe11fefe80dbce65199c662a727
Michael Frey <mail@mfrey.net>
2015-06-21 17:34:20 +0200

    minor changes to ARA unit tests

--- tests/unittests/tests-ara/tests-ara.c
+++ tests/unittests/tests-ara/tests-ara.c
@@ -20,36 +20,34 @@ static void test_ara_routing_table_initialized(void)
 
 static void test_ara_routing_table_get_entry(void)
 {
-    ara_routing_entry_t *entry = malloc(sizeof(ara_routing_entry_t));
-    
-    if (entry) {
-        memset(entry, 0, sizeof(ara_routing_entry_t));
+    ara_routing_entry_t entry; 
+    memset(&entry, 0, sizeof(ara_routing_entry_t));
 
-        struct netaddr address = { {
+    struct netaddr address = { {
             0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-            0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
-            }, AF_INET6, 128
-        };
+            0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0xff
+        }, AF_INET6, 128
+    };
 
-        /* the entry should not be in the routing table */
-        TEST_ASSERT_EQUAL_INT(true, ara_routing_table_get_entry(&address) == NULL); 
+    /* the entry should not be in the routing table */
+    TEST_ASSERT_EQUAL_INT(true, ara_routing_table_get_entry(&address) == NULL); 
 
-        entry->destination = &address; 
+    entry.destination = &address;
+    /* add the entry to the routing table */
+    ara_routing_table_add_entry(&entry);
 
-        /* add the entry to the routing table */
-        ara_routing_table_add_entry(entry);
+    /* the entry should be in the routing table */
+    TEST_ASSERT_EQUAL_INT(true, ara_routing_table_get_entry(&address) != NULL); 
 
-        /* the entry should be in the routing table */
-        TEST_ASSERT_EQUAL_INT(true, ara_routing_table_get_entry(&address) != NULL); 
+    ara_routing_table_del_entry(&entry);
 
-        ara_routing_table_del_entry(entry);
-        free(entry);
-    }
+    TEST_ASSERT_EQUAL_INT(0, ara_routing_table_size()); 
 }
 
 static void test_ara_routing_table_add_entry(void)
 {
     ara_routing_entry_t entry; 
+    memset(&entry, 0, sizeof(ara_routing_entry_t));
 
     struct netaddr address = { {
             0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
@@ -57,11 +55,30 @@ static void test_ara_routing_table_add_entry(void)
         }, AF_INET6, 128
     };
 
+    struct netaddr another_address = { {
+            0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07
+        }, AF_INET6, 128
+    };
+
+    /* add first entry */
     entry.destination = &address;
     ara_routing_table_add_entry(&entry);
 
+    TEST_ASSERT_EQUAL_INT(1, ara_routing_table_size()); 
     TEST_ASSERT_EQUAL_INT(true, ara_routing_table_entry_exists(&address)); 
-    TEST_ASSERT_EQUAL_INT(false, ara_routing_table_entry_exists(&address)); 
+
+    /* add another entry with a different address */
+    entry.destination = &another_address;
+    ara_routing_table_add_entry(&entry);
+
+    TEST_ASSERT_EQUAL_INT(2, ara_routing_table_size()); 
+    TEST_ASSERT_EQUAL_INT(true, ara_routing_table_entry_exists(&another_address)); 
+
+    ara_routing_table_del_entry(&entry);
+
+    TEST_ASSERT_EQUAL_INT(1, ara_routing_table_size()); 
+    TEST_ASSERT_EQUAL_INT(false, ara_routing_table_entry_exists(&another_address)); 
 }
 
 static void test_ara_cum_sum(void)
