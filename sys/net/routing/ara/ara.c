667d6e1059c985bbb6ad7b28029408c1112d55ad
Michael Frey <mail@mfrey.net>
2015-10-07 19:54:48 +0200

    added stub for event loop for ARA

--- sys/net/routing/ara/ara.c
+++ sys/net/routing/ara/ara.c
@@ -17,37 +17,152 @@
  */
 
 #include "ara.h"
+#include "msg.h"
 #include "forwarding.h"
+#include "routingtable.h"
 
-void ara_init(void)
+
+kernel_pid_t ara_pid = KERNEL_PID_UNDEF;
+
+static char _ara_stack[ARA_STACK_SIZE];
+/* main event loop for ARA */
+static void *ara_event_loop(void *args);
+
+kernel_pid_t ara_init(void)
 {
+    /* initialize the ara thread */
+    if (ara_pid == KERNEL_PID_UNDEF) {
+        ara_pid = thread_create(_ara_stack, sizeof(_ara_stack), ARA_PRIO,
+                CREATE_STACKTEST, ara_event_loop, NULL, "ara");
+    }
     /* initialize the probablistic forwarding */
     ara_forwarding_init();
+
+    return ara_pid;
+}
+
+static void *ara_event_loop(void *args)
+{
+    msg_t message; 
+    msg_t message_queue[ARA_MSG_QUEUE_SIZE];
+
+    if(msg_init_queue(message_queue, ARA_MSG_QUEUE_SIZE) == -1){
+
+    }
+
+    while (1) {
+        msg_receive(&message);
+
+        switch (message.type) {
+            default:
+                break;
+        }
+    }
+
+    return NULL;
 }
 
 void ara_send_packet(ara_packet_t* packet)
 {
+    ara_routing_table_trigger_evaporation();
+
     if (packet->ttl > 0) {
         struct netaddr* destination = packet->destination;
 
         if (ara_is_route_discovery_running(destination)) {
+            /* trap packet */
 
-        } else if(ara_is_deliverable(destination)) {
+        } else if(ara_routing_table_is_deliverable(destination)) {
+            /* get next hop */
+            ara_next_hop_t* next_hop = ara_get_next_hop(destination);
+            /* build packet */
+
+            /* reinforce pheromone value */
+//            float phi = ara_reinforce_pheromone(next_hop->address, destination, NULL);
+#if ENABLE_DEBUG
+            struct netaddr_str buf;
+            printf("there is no routing table entry for %s\n", netaddr_to_string(&buf, destination));
+#endif
+   
+            /* send packet */
+
+        } else {
+            /* packet is not deliverable and no route discovery is running */
+            if (ara_is_local_address(packet->source)) {
+                /* packet from is not deliverable */
+
+                /* trap packet */
+
+                /* */
+                ara_start_new_route_discovery(packet);
+            } else {
+                ara_handle_non_source_route_discovery(packet);
+            }
 
         }
     /* handle packet with zero ttl  */
     } else {
-
-
+        ara_handle_packet_with_zero_ttl(packet);
     }
 }
+
+ara_next_hop_t* ara_get_next_hop(struct netaddr* destination)
+{
+    ara_next_hop_t* result = NULL;
+    /**
+     * the forwarding functions require the routing table entry with all
+     * possible next hops in order to (probalistic) chose from all options.
+     * hence, we determine the corresponding entry for a given address.
+     */
+    ara_routing_entry_t* entry = ara_routing_table_get_entry(destination);
+
+    if (entry != NULL) {
+       switch (ARA_FORWARDING_MODE) {
+           default:
+               result = ara_stochastic_forwarding(entry);
+       }
+    } else {
+#if ENABLE_DEBUG
+        struct netaddr_str buf;
+        printf("there is no routing table entry for %s\n", netaddr_to_string(&buf, destination));
+#endif
+    }
+
+    return result;
+}
+
+
+void ara_reinforce_pheromone(struct netaddr* next_hop, struct netaddr* destination, void* interface)
+{
+    /* 
+    float current_value = ara_routing_table_get_pheromone_value(destination, next_hop);
+    float new_pheromone_value = ara_reinforcement_linear_compute(current_pheromone_value);
+    ara_routing_table_update(destination, next_hop, interface, new_pheromone_value);
+    return new_pheromone_value;
+     */
+}
+
 
 bool ara_is_route_discovery_running(struct netaddr* destination)
 {
     return false;
 }
 
-bool ara_is_deliverable(struct netaddr* destination)
+void ara_handle_non_source_route_discovery(ara_packet_t *packet)
 {
-    return false;
+    // Dropping packet ....
+    ara_broadcast_route_failure(packet->destination);
+    //free packet
 }
+
+void ara_broadcast_route_failure(struct netaddr* address)
+{
+
+}
+
+void ara_handle_packet_with_zero_ttl(ara_packet_t* packet)
+{
+    // Dropping packet ...
+    // free(packet);
+}
+
--- sys/net/routing/ara/ara.h
+++ sys/net/routing/ara/ara.h
@@ -21,12 +21,64 @@
 
 #include "stdbool.h"
 #include "packet.h"
+#include "routingtable.h"
+
+#include "kernel_types.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void ara_init(void);
+/**
+ * @brief   Default stack size to use for the ARA thread
+ */
+#ifndef ARA_STACK_SIZE
+#define ARA_STACK_SIZE (THREAD_STACKSIZE_DEFAULT)
+#endif
+
+/**
+ * @brief   Default priority for the ARA thread
+ */
+#ifndef ARA_PRIO
+#define ARA_PRIO      (THREAD_PRIORITY_MAIN - 3)
+#endif
+
+/**
+ * @brief   Default evaporation interval 
+ */
+#ifndef ARA_EVAPORATION_INTERVAL
+#define ARA_EvAPORATION_INTERVAL     (0)
+#endif
+
+/**
+ * TODO: think about a better way to switch between forwarding modes
+ */
+#ifndef ARA_FORWARDING_MODE
+#define ARA_FORWARDING_MODE (0)
+#endif
+
+/**
+ * @brief   Default message queue size to use for the ARA thred.
+ */
+#ifndef ARA_MSG_QUEUE_SIZE
+#define ARA_MSG_QUEUE_SIZE  (8U)
+#endif
+
+/**
+ * @brief   Default parameter for linear reinforcement of pheromone values
+ */
+#ifndef ARA_LINEAR_REINFORCEMENT_DELTA
+#define ARA_LINEAR_REINFORCEMNET_DELTA (0)
+#endif
+
+/**
+ * TODO: think about a better way to switch between evaporation policies
+ */
+#ifndef ARA_EVAPORATION_POLICY
+#define ARA_EVAPORATION_POLICY (0)
+#endif
+
+kernel_pid_t ara_init(void);
 
 void ara_send_packet(ara_packet_t* packet);
 
@@ -36,7 +88,21 @@ void ara_receive_packet();
 
 bool ara_is_route_discovery_running(struct netaddr* destination);
 
-bool ara_is_deliverable(struct netaddr* destination);
+void ara_handle_non_source_route_discovery(ara_packet_t* packet);
+
+void ara_handle_packet_with_zero_ttl(ara_packet_t* packet);
+
+void ara_broadcast_route_failure(struct netaddr* address);
+
+bool ara_is_local_address(struct netaddr* address);
+
+void ara_start_new_route_discovery(ara_packet_t* packet);
+
+bool ara_is_route_discovery_running(struct netaddr* address);
+
+ara_next_hop_t* ara_get_next_hop(struct netaddr* destination);
+
+void ara_reinforce_pheromone(struct netaddr* next_hop, struct netaddr* destination, void* interface);
 
 #ifdef  __cplusplus
 }
