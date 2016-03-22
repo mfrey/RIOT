0032e6496bb8ca6b051513f6d098e21bb8c482da
Michael Frey <mail@mfrey.net>
2016-03-22 09:18:19 +0100

    added initial stub for mqtt-sn

---
+++ sys/net/application_layer/mqttsn/mqttsn.c
@@ -0,0 +1,377 @@
+#include "mqttsn.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+#include <byteorder.h>
+
+#include "net/gnrc.h"
+#include "net/gnrc/ipv6.h"
+#include "net/gnrc/udp.h"
+#include "net/gnrc/pktdump.h"
+
+static uint16_t msg_id = 1;
+static bool forward_encapsulation = false;
+static ipv6_addr_t source;
+static uint16_t source_port;
+static ipv6_addr_t destination;
+static uint16_t destination_port;
+uint8_t wireless_node_id[MQTTSN_MAX_WIRELESS_NODE_ID_LENGTH];  
+uint8_t wireless_node_length;
+
+static uint8_t mqttsn_get_qos_flag(int8_t qos) 
+{
+    switch (qos) {
+        case 0:
+            return MQTTSN_FLAG_QOS_0;
+        case 1:
+            return MQTTSN_FLAG_QOS_1;
+        case 2:
+            return MQTTSN_FLAG_QOS_2;
+        case -1:
+            return MQTTSN_FLAG_QOS_N1;
+        default:
+            return 0;
+    }
+}
+
+void mqttsn_init(ipv6_addr_t src, uint16_t src_port, ipv6_addr_t dest, uint16_t dest_port, bool enable_forward_encapsulation) 
+{
+    source = src;
+    destination = dest;
+    source_port = src_port;
+    destination_port = dest_port;
+
+    forward_encapsulation = enable_forward_encapsulation;
+
+    
+}
+
+
+void mqttsn_connect(const char* client_identifier, size_t client_identifier_length, uint16_t duration, bool clean_session) {
+    /** 
+     * check if client identifier is not set or larger than 23 bytes (the maximum
+     * length) 
+     */
+    if (!client_identifier || client_identifier_length > 23) {
+#if ENABLE_DEBUG 
+        printf("the client identifier is not set or too large!\n");
+#endif 
+        return;
+    } 
+
+    /** actual length of the packet */
+    size_t length = 0x06 + client_identifier_length;
+    /** raw packet */ 
+    char data[length];
+    /** wrap raw packet around connect packet structure */
+    mqttsn_msg_connect_t* connect_packet = (mqttsn_msg_connect_t*)data;
+    memset(&connect_packet, 0, sizeof(connect_packet));
+
+    /** set the parameters of the packet */
+    connect_packet->length = 0x06 + client_identifier_length;
+    connect_packet->msg_type = MQTTSN_TYPE_CONNECT;
+    connect_packet->flags = 0;
+
+    if (clean_session) {
+        connect_packet->flags = MQTTSN_FLAG_CLEAN_SESSION;
+    }
+
+    connect_packet->protocol_identifier = 0x01;
+    connect_packet->duration = HTONS(duration);
+
+    size_t size = strnlen(client_identifier, 23); 
+
+    if (size != client_identifier_length) {
+#if ENABLE_DEBUG 
+        printf("there is a mismatch in the size of the client identifier. got: %d but expected: %d !\n", size, client_identifier_length);
+#endif 
+        return;
+    }
+    
+    memcpy((connect_packet+1), client_identifier, size);
+
+    // TODO
+    if (duration) {
+
+    }
+
+    /** send the packet */
+    mqttsn_send(connect_packet);
+}
+
+void mqttsn_disconnect(void) {
+    /** prepare the disconnect packet */
+    mqttsn_msg_disconnect_t disconnect_packet;
+    memset(&disconnect_packet, 0, sizeof(disconnect_packet));
+
+    /** set the members */
+    disconnect_packet.msg_type = MQTTSN_TYPE_DISCONNECT;
+    disconnect_packet.length = 0x02;
+
+    // TODO: what's about the duration
+
+    /** send the packet */
+    mqttsn_send(&disconnect_packet);
+}
+
+void mqttsn_ping_request(void) 
+{
+    char ping_request_packet[2];
+    /** the length of the packet */
+    ping_request_packet[0] = 2;
+    /** the type of the packet */
+    ping_request_packet[1] = MQTTSN_TYPE_PINGREQ;
+    /** send the packet */
+    mqttsn_send(&ping_request_packet);
+}
+
+void mqttsn_register(const char *topic_name, size_t topic_length, uint16_t topic_id) 
+{
+    if (!topic_name || (topic_length > MQTTSN_MAX_TOPIC_LENGTH)) {
+#if ENABLE_DEBUG 
+        printf("the topic name is not set or too large!\n");
+#endif 
+        return;
+    }
+
+    /** actual length of the packet */
+    size_t length = 0x06 + topic_length;
+    /** raw packet */ 
+    char data[length];
+    /** wrap raw packet around connect packet structure */
+    mqttsn_msg_register_t* register_packet = (mqttsn_msg_register_t*)data;
+    memset(register_packet, 0, sizeof(mqttsn_msg_register_t));
+
+    register_packet->msg_type = MQTTSN_TYPE_REGISTER;
+    /** topic id is always 0x000 for clients */
+    register_packet->topic_id = topic_id;
+    /** get a new sequence number */
+    register_packet->msg_id = HTONS(msg_id++);
+    /** set the size of the packet */
+    register_packet->length = 0x06 + topic_length;
+
+    size_t size = strnlen(topic_name, MQTTSN_MAX_TOPIC_LENGTH); 
+
+    if (size != topic_length) {
+#if ENABLE_DEBUG 
+        printf("there is a mismatch in the size of the topic length. got: %d but expected: %d !\n", size, topic_length);
+#endif 
+        return;
+    }
+
+    /** set the topic */
+    memcpy((register_packet+1), topic_name, size);
+    /** send the packet */
+    mqttsn_send(register_packet);
+}
+
+void mqttsn_register_acknowledgement(uint16_t topic_id, uint16_t msg_id) 
+{
+    mqttsn_msg_register_acknowledgement_t register_acknowledgement_packet;
+    memset(&register_acknowledgement_packet, 0, sizeof(mqttsn_msg_register_acknowledgement_t));
+
+    register_acknowledgement_packet.msg_type = MQTTSN_TYPE_REGACK;
+    register_acknowledgement_packet.topic_id = HTONS(topic_id);
+    register_acknowledgement_packet.msg_id = HTONS(msg_id);
+    // TODO: check when we're going to reject a REGISTER message
+    register_acknowledgement_packet.return_code = MQTTSN_RETURN_CODE_ACCEPTED;
+    register_acknowledgement_packet.length = 0x07;
+
+    mqttsn_send(&register_acknowledgement_packet);
+}
+
+void mqttsn_subscribe_topic_id(uint16_t topic_identifier, int8_t qos) 
+{
+    /** actual length of the packet */
+    size_t length = 0x06 + sizeof(topic_identifier);
+    /** raw packet */ 
+    char data[length];
+    mqttsn_msg_subscribe_t *subscribe_topic_packet = (mqttsn_msg_subscribe_t*)data;
+    memset(subscribe_topic_packet, 0, sizeof(mqttsn_msg_subscribe_t));
+
+    subscribe_topic_packet->msg_type = MQTTSN_TYPE_SUBSCRIBE;
+    subscribe_topic_packet->flags = 0x00;
+    subscribe_topic_packet->flags += mqttsn_get_qos_flag(qos);
+    subscribe_topic_packet->flags += MQTTSN_TOPIC_TYPE_PRE_DEFINED;
+    subscribe_topic_packet->msg_id = HTONS(msg_id++);
+    subscribe_topic_packet->length = 0x05 + 2;
+
+    topic_identifier = HTONS(topic_identifier);
+
+    /** set the topic id */
+    memcpy((subscribe_topic_packet+1), topic_identifier, sizeof(topic_identifier));
+
+    mqttsn_send(subscribe_topic_packet);
+}
+
+void mqttsn_subscribe_topic_name(const char* topic_name, size_t topic_length, int8_t qos) 
+{
+    size_t size = strnlen(topic_name, MQTTSN_MAX_TOPIC_LENGTH);
+
+    if (size != topic_length) {
+#if ENABLE_DEBUG 
+        printf("there is a mismatch in the size of the topic length. got: %d but expected: %d !\n", size, topic_length);
+#endif 
+        return;
+    }
+
+    /** actual length of the packet */
+    size_t length = 0x06 + size;
+    /** raw packet */ 
+    char data[length];
+    mqttsn_msg_subscribe_t *subscribe_topic_packet = (mqttsn_msg_subscribe_t*)data;
+    memset(subscribe_topic_packet, 0, sizeof(mqttsn_msg_subscribe_t));
+
+    subscribe_topic_packet->msg_type = MQTTSN_TYPE_SUBSCRIBE;
+    subscribe_topic_packet->msg_id = HTONS(msg_id++);
+    subscribe_topic_packet->length = 0x05 + size;
+
+    subscribe_topic_packet->flags = 0x00;
+    subscribe_topic_packet->flags += mqttsn_get_qos_flag(qos);
+    if (size == 2) {
+        subscribe_topic_packet->flags += MQTTSN_TOPIC_TYPE_SHORT;
+    } else {
+        subscribe_topic_packet->flags += MQTTSN_TOPIC_TYPE_NAME;
+    }
+        
+    /** set the topic id */
+    memcpy((subscribe_topic_packet+1), topic_name, size);
+
+    mqttsn_send(subscribe_topic_packet);
+}
+
+void mqttsn_publish(uint16_t topic_identifier, uint8_t topic_type, const void* data, size_t payload_size, int8_t qos, uint8_t retain) 
+{
+    if (!data || (payload_size > (MQTTSN_MAX_PACKET_LENGTH-7))) {
+#if ENABLE_DEBUG 
+        printf("the payload is not set or too large!\n");
+#endif 
+        return;
+    }
+
+    size_t size = strnlen(data, MQTTSN_MAX_PACKET_LENGTH-7);
+
+    if (size != payload_size) {
+#if ENABLE_DEBUG 
+        printf("there is a mismatch in the size of the topic length. got: %d but expected: %d !\n", size, topic_length);
+#endif 
+        return;
+    }
+
+    char packet[size+7];
+    mqttsn_msg_publish_t *publish_packet = (mqttsn_msg_publish_t*)packet;
+
+    publish_packet->msg_type = MQTTSN_TYPE_PUBLISH;
+
+    publish_packet->flags = 0x00;
+    publish_packet->flags +=  mqttsn_get_qos_flag(qos);
+    if (retain) {
+        publish_packet->flags += MQTTSN_FLAG_RETAIN;
+    }
+
+    publish_packet->flags += (topic_type & 0x3);
+    publish_packet->msg_id = HTONS(msg_id++);
+    publish_packet->topic_id = HTONS(topic_identifier);
+    publish_packet->length = 0x07 + size;
+
+    memcpy((publish_packet+1), data, size);
+
+    mqttsn_send(publish_packet);
+}
+
+
+void mqttsn_send(void *packet)
+{
+    /** the first octet always specifies the length of the packet */
+    size_t length = ((uint8_t*)packet)[0];
+
+    gnrc_pktsnip_t *payload, *udp, *ip;
+
+    // TODO: check if it makes sense to create a nettype for MQTT-SN
+    payload = gnrc_pktbuf_add(NULL, packet, length, GNRC_NETTYPE_UNDEF);
+    if (!payload) {
+#if ENABLE_DEBUG 
+        printf("could not add payload to packet buffer\n");
+#endif 
+        return;
+    }
+
+    /** check if forward encapsulation is enabled */
+    if (forward_encapsulation) {
+        /** the packet needs to be wrapped according to the specification */
+        mqttsn_msg_forward_encapsulation_t forward_encapsulation_packet;
+        /** set the wireless node id */
+        memcpy(&(forward_encapsulation_packet.wireless_node_id), wireless_node_id, wireless_node_length);
+        /** set the packet type */
+        forward_encapsulation_packet.msg_type = MQTTSN_TYPE_ENCMSG;
+        // TODO
+        forward_encapsulation_packet.ctrl = 0;
+        /** set the packet length of the forward encapsulation packet */
+        forward_encapsulation_packet.length = wireless_node_length + 3;
+
+        /** append forward encapsulation packet to */
+        gnrc_pktsnip_t *forward_encapsulation_payload = gnrc_pktbuf_add(payload, (void*)&forward_encapsulation_packet, 
+                forward_encapsulation_packet.length, GNRC_NETTYPE_UNDEF);
+        /** */
+        if (!forward_encapsulation_payload) {
+#if ENABLE_DEBUG 
+            printf("could not add forward encapsulation payload to packet buffer\n");
+#endif 
+            gnrc_pktbuf_release(payload);
+            return;
+        }
+
+        /** build udp header */
+        udp = gnrc_udp_hdr_build(forward_encapsulation_payload, source_port, 2, destination_port, 2);
+    } else {
+        /** build udp header */
+        udp = gnrc_udp_hdr_build(payload, source_port, 2, destination_port, 2);
+    }
+
+    if (!udp) {
+#if ENABLE_DEBUG 
+        printf("could not add udp header to packet buffer\n");
+#endif 
+        gnrc_pktbuf_release(payload);
+        return;
+    }
+
+    /** build ip header */
+    ip = gnrc_ipv6_hdr_build(udp, NULL, 0, (uint8_t *)&destination, sizeof(destination));
+    if (!ip) {
+#if ENABLE_DEBUG 
+        printf("could not add ip header to packet buffer\n");
+#endif 
+        gnrc_pktbuf_release(udp);
+        return;
+    }
+
+    /** send packet */
+    if (!gnrc_netapi_dispatch_send(GNRC_NETTYPE_UDP, GNRC_NETREG_DEMUX_CTX_ALL, ip)) {
+#if ENABLE_DEBUG 
+        printf("Error: unable to locate UDP thread\n");
+#endif 
+        gnrc_pktbuf_release(ip);
+        return;
+    }
+}
+
+uint8_t mqttsn_check_wireless_node_id(uint8_t *node_id, uint8_t node_length) {
+    if (node_length > MQTTSN_MAX_WIRELESS_NODE_ID_LENGTH) {
+#if ENABLE_DEBUG 
+        printf("the size of the wireless node identifier is too large. got: %d but maximum length is: %d !\n", node_length, MQTTSN_MAX_WIRELESS_NODE_ID_LENGTH);
+#endif 
+        return -1;
+    }
+
+   // if ((!wireless_node_id) || (wireless_node_length == 0)) {
+        // TODO
+//        snprintf(wireless_node_id, sizeof(wireless_node_id)-1, "%X", mqttsn_get_wireless_node_id(void));
+  //      wireless_node_length = strnlen((char*)packet->wireless_node_id, MQTTSN_MAX_WIRELESS_NODE_ID_LENGTH);
+   // } else {
+//        memcpy(packet->wireless_node_id, wireless_node_id, wireless_node_length);
+  //  }
+    return 0;
+}
---
+++ sys/net/application_layer/mqttsn/mqttsn.h
@@ -0,0 +1,266 @@
+#ifndef MQTTSN_H_
+#define MQTTSN_H_
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "net/ipv6/addr.h"
+
+/**
+ * The maxmimum packet length of a MQTTSN message in bytes.
+ */
+#define MQTTSN_MAX_PACKET_LENGTH (255)
+
+/**
+ * The maximum topic length. The constant '6' denotes the remaining 6 bytes
+ * needed for the message.
+ */
+#define MQTTSN_MAX_TOPIC_LENGTH (MQTTSN_MAX_PACKET_LENGTH - 6)
+
+/**
+ * The maximum length of the wireless node id (which is used in the 
+ * forwarder encapsulation).
+ */
+#define MQTTSN_MAX_WIRELESS_NODE_ID_LENGTH (252)
+
+typedef enum {
+    MQTTSN_TYPE_ADVERTISE       = 0x00,
+    MQTTSN_TYPE_SEARCHGW        = 0x01,
+    MQTTSN_TYPE_GWINFO          = 0x02,
+    MQTTSN_TYPE_CONNECT         = 0x04,
+    MQTTSN_TYPE_CONNACK         = 0x05,
+    MQTTSN_TYPE_WILLTOPICREQ    = 0x06,
+    MQTTSN_TYPE_WILLTOPIC       = 0x07,
+    MQTTSN_TYPE_WILLMSGREQ      = 0x08,
+    MQTTSN_TYPE_WILLMSG         = 0x09,
+    MQTTSN_TYPE_REGISTER        = 0x0A,
+    MQTTSN_TYPE_REGACK          = 0x0B,
+    MQTTSN_TYPE_PUBLISH         = 0x0C,
+    MQTTSN_TYPE_PUBACK          = 0x0D,
+    MQTTSN_TYPE_PUBCOMP         = 0x0E,
+    MQTTSN_TYPE_PUBREC          = 0x0F,
+    MQTTSN_TYPE_PUBREL          = 0x10,
+    MQTTSN_TYPE_SUBSCRIBE       = 0x12,
+    MQTTSN_TYPE_SUBACK          = 0x13,
+    MQTTSN_TYPE_UNSUBSCRIBE     = 0x14,
+    MQTTSN_TYPE_UNSUBACK        = 0x15,
+    MQTTSN_TYPE_PINGREQ         = 0x16,
+    MQTTSN_TYPE_PINGRESP        = 0x17,
+    MQTTSN_TYPE_DISCONNECT      = 0x18,
+    MQTTSN_TYPE_WILLTOPICUPD    = 0x1A,
+    MQTTSN_TYPE_WILLTOPICRESP   = 0x1B,
+    MQTTSN_TYPE_WILLMSGUPD      = 0x1C,
+    MQTTSN_TYPE_WILLMSGRESP     = 0x1D,
+    MQTTSN_TYPE_ENCMSG          = 0xFE
+} mqttsn_msg_t;
+
+typedef enum {
+    MQTTSN_RETURN_CODE_ACCEPTED               = 0x00,
+    MQTTSN_RETURN_CODE_REJECTED_CONGESTION    = 0x01,
+    MQTTSN_RETURN_CODE_REJECTED_INVALID_TOPIC = 0x02,
+    MQTTSN_RETURN_CODE_REJECTED_NOT_SUPPORTED = 0x03
+} mqttsn_return_code;
+
+
+typedef enum {
+    MQTTSN_FLAG_RETAIN        = 0x1 << 4,
+    MQTTSN_FLAG_WILL          = 0x1 << 3,
+    MQTTSN_FLAG_CLEAN_SESSION = 0x1 << 2,
+    MQTTSN_FLAG_DUP           = 0x1 << 7,
+    MQTTSN_FLAG_QOS_0         = 0x0 << 5,
+    MQTTSN_FLAG_QOS_1         = 0x1 << 5,
+    MQTTSN_FLAG_QOS_2         = 0x2 << 5,
+    MQTTSN_FLAG_QOS_N1        = 0x3 << 5
+} mqttsn_flag_t;
+
+/**
+ * Topic types define which type of information are included in a SUBSCRIBE
+ * message.
+ */
+typedef enum {
+    MQTTSN_TOPIC_TYPE_NAME = 0x00,       /**< topic name */
+    MQTTSN_TOPIC_TYPE_PRE_DEFINED= 0x01, /**< pre-defined topic id */
+    MQTTSN_TOPIC_TYPE_SHORT = 0x10,      /**< short topic name */
+    MQTTSN_TOPIC_TYPE_RESERVED = 0x11    /**< reserved */
+} mqttsn_topic_t;
+
+/**
+ * The DISCONNECT message is sent by a client to indicate that it wants
+ * to close the connection. The gateway will acknowledge the receipt of
+ * the message by returning a DISCONNECT message to the client. A 
+ * DISCONNECT message might also be sent by a gateway or server in case
+ * of an error. If the duration field is set by a client, the field 
+ * indicates when the clients intents to go to the 'asleep' state. The 
+ * gateway replies as well without a duration field.
+ */
+typedef struct __attribute__((packed)) {
+    uint8_t length;
+    uint8_t msg_type;
+    uint16_t duration;
+} mqttsn_msg_disconnect_t;
+
+typedef struct __attribute__((packed)) {
+    uint8_t length;
+    uint8_t msg_type;
+    uint8_t flags;
+    uint8_t protocol_identifier;
+    uint16_t duration;
+} mqttsn_msg_connect_t;
+
+typedef struct __attribute__((packed)) {
+    uint8_t length;
+    uint8_t msg_type;
+    uint16_t topic_id;
+    uint16_t msg_id;
+} mqttsn_msg_register_t;
+
+/**
+ *
+ */
+typedef struct __attribute__((packed)) {
+    uint8_t length;
+    uint8_t msg_type;
+    uint16_t topic_id;
+    uint16_t msg_id;
+    uint8_t return_code;
+} mqttsn_msg_register_acknowledgement_t;
+
+typedef struct __attribute__((packed)) {
+    uint8_t length;
+    uint8_t msg_type;
+    uint8_t flags;
+    uint16_t msg_id;
+} mqttsn_msg_subscribe_t;
+
+typedef struct __attribute__((packed)) {
+    uint8_t length;
+    uint8_t msg_type;
+    uint8_t flags;
+    uint16_t topic_id;
+    uint16_t msg_id;
+} mqttsn_msg_publish_t;
+
+typedef struct __attribute__((packed)) {
+    /** length of the packet up till the end of the wireless node id */
+    uint8_t length;            
+    /** type of the message */
+    uint8_t msg_type;              
+    /** control information exchanged between forwarder/gateway */
+    uint8_t ctrl;           
+    /** the wireless node identifier of this device */
+    uint8_t wireless_node_id[MQTTSN_MAX_WIRELESS_NODE_ID_LENGTH];  
+    /** the actual wrapped MQTT-SN message */
+    char msg[MQTTSN_MAX_PACKET_LENGTH];
+} mqttsn_msg_forward_encapsulation_t;
+
+/**
+ * The CONNACK message is sent by the server in response to a
+ * connection request from a client. 
+ */
+typedef struct __attribute__((packed)) {
+    uint8_t length;      
+    uint8_t msg_type;    /**< */
+    uint8_t return_code; /**< */
+} mqttsn_msg_connack;
+
+/**
+ * The WILLTOPICREQ message is sent by the gateway to request a
+ * client for sending the Will topic.
+ */
+typedef struct __attribute__((packed)) {
+    uint8_t length;      /**< */
+    uint8_t msg_type;    /**< */
+} mqttsn_msg_willtopicreq_t;
+
+/**
+ * The WILLTOPIC message is sent by a client as response to the 
+ * WILLTOPICREQ message for transferring its WILL topic name to
+ * the gateway.
+ */
+typedef struct __attribute__((packed)) {
+    uint8_t length;      /**< */
+    uint8_t msg_type;    /**< */
+    uint8_t flags;       /**< */
+} mqttsn_msg_willtopic_t;
+
+
+void mqttsn_init(ipv6_addr_t src, uint16_t src_port, ipv6_addr_t dest, uint16_t dest_port, bool enable_forward_encapsulation);
+
+/**
+ * Sends a CONNECTION message in order to set up a connection.
+ *
+ * @param[in] client_identifier The identifier of this client.
+ * @param[in] client_identifier_length The length of the client identifier
+ * @param[in] duration The value of the keep alive timer
+ * @param[in] clean_session Flag indicating if this is going to be a clean
+ * session (as in MQTT)
+ */
+void mqttsn_connect(const char* client_identifier, size_t client_identifier_length, uint16_t duration, bool clean_session);
+
+/**
+ * Sends a DISCONNECT message in order to indicate that the client wants to
+ * disconnect.
+ */    
+void mqttsn_disconnect(void);
+
+/**
+ * Sends a PINGREQ message in order to show if a gateway or client is still
+ * alive.
+ */    
+void mqttsn_ping_request(void);
+
+/**
+ * Requests a topic id value for the given topic name (REGISTER message).
+ *
+ * @param[in] topic_name The name of the topic
+ * @param[in] topic_length The length of the topic
+ * @param[in] topic_id The topic id value assigned by the gateway (only set by
+ * gateways.
+ */
+void mqttsn_register(const char *topic_name, size_t topic_length, uint16_t topic_id);
+
+/**
+ * Sends a REGACK message as an acknowledgment to the receipt and processing of
+ * a REGISTER message. 
+ *
+ * @param[in] topic_id The value which is used as topic id in the PUBLISH
+ * messages.
+ * @param[in] msg_id The same value as the one received in the REGISTER message.
+ */
+void mqttsn_register_acknowledgement(uint16_t topic_id, uint16_t msg_id);
+
+/**
+ * Subscribes to a given topic id (SUBSCRIBE message).
+ *
+ * @param[in] topic_identifier The topic identifier
+ * @param[in] qos The QoS level of this topic
+ */
+void mqttsn_subscribe_topic_id(uint16_t topic_identifier, int8_t qos);
+
+/**
+ * Subscribes to a given topic name (SUBSCRIBE message).
+ *
+ * @param[in] topic_name The topic name a clients wants to subscribe to
+ * @param[in] topic_length The size of the the topic name
+ * @param[in] qos The QoS level of this topic
+ */
+void mqttsn_subscribe_topic_name(const char* topic_name, size_t topic_length,  int8_t qos);
+
+/**
+ * Publishes data for a given topic (PUBLISH message).
+ *
+ * @param[in] topic_identifier The topic identifier for which the data is
+ * published.
+ * @param[in] topic_type The type of the given topic_identifier
+ * @param[in] data The published data
+ * @param[in] payload_size The size of the published data
+ * @param[in] qos The QoS level for this PUBLISH message
+ * @param[in] retain The retain flag of this message
+ */
+void mqttsn_publish(uint16_t topic_identifier, uint8_t topic_type, const void* data, size_t payload_size, int8_t qos, uint8_t retain);
+
+void mqttsn_send(void *packet);
+
+uint8_t mqttsn_check_wireless_node_id(uint8_t *wireless_node_id, uint8_t wireless_node_length);
+#endif 
