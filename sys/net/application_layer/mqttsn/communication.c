70ddd026c4601b0b5240ae36387946cc43c86da1
Michael Frey <mail@mfrey.net>
2016-03-25 20:09:12 +0100

    added udp-based send method for mqtt-sn

---
+++ sys/net/application_layer/mqttsn/communication.c
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2016 Michael Frey <mail@mfrey.net>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     mqtt-sn
+ * @{
+ *
+ * @file        communication.c
+ * @brief       ARA routing protocol
+ *
+ * @author      Michael Frey <mail@mfrey.net>
+ */
+
+#include "communication.h" 
+#include "mqttsn.h"
+#include "constants.h"
+
+#include "net/gnrc.h"
+#include "net/gnrc/ipv6.h"
+#include "net/gnrc/udp.h"
+#include "net/gnrc/pktdump.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static bool forward_encapsulation = false;
+static ipv6_addr_t source;
+static uint16_t source_port;
+static ipv6_addr_t destination;
+static uint16_t destination_port;
+uint8_t wireless_node_id[MQTTSN_MAX_WIRELESS_NODE_ID_LENGTH];  
+uint8_t wireless_node_length;
+
+void mqttsn_communication_init(ipv6_addr_t src, uint16_t src_port, ipv6_addr_t dest, uint16_t dest_port, bool enable_forward_encapsulation)
+{
+    source = src;
+    destination = dest;
+    source_port = src_port;
+    destination_port = dest_port;
+
+    forward_encapsulation = enable_forward_encapsulation;
+}
+
+void mqttsn_communication_send_udp(void *packet)
+{
+    /** the first octet always specifies the length of the packet */
+    size_t length = ((uint8_t*)packet)[0];
+
+    gnrc_pktsnip_t *payload, *udp, *ip;
+
+    // TODO: check if it makes sense to create a nettype for MQTT-SN
+    payload = gnrc_pktbuf_add(NULL, packet, length, GNRC_NETTYPE_UNDEF);
+    if (!payload) {
+#if ENABLE_DEBUG 
+        printf("could not add payload to packet buffer\n");
+#endif 
+        return;
+    }
+
+    /** check if forward encapsulation is enabled */
+    if (forward_encapsulation) {
+        /** the packet needs to be wrapped according to the specification */
+        mqttsn_msg_forward_encapsulation_t forward_encapsulation_packet;
+        /** set the wireless node id */
+        memcpy(&(forward_encapsulation_packet.wireless_node_id), wireless_node_id, wireless_node_length);
+        /** set the packet type */
+        forward_encapsulation_packet.header.msg_type = MQTTSN_TYPE_ENCMSG;
+        // TODO
+        forward_encapsulation_packet.ctrl = 0;
+        /** set the packet length of the forward encapsulation packet */
+        forward_encapsulation_packet.header.length = wireless_node_length + 3;
+
+        /** append forward encapsulation packet to */
+        gnrc_pktsnip_t *forward_encapsulation_payload = gnrc_pktbuf_add(payload, (void*)&forward_encapsulation_packet, 
+                forward_encapsulation_packet.header.length, GNRC_NETTYPE_UNDEF);
+        /** */
+        if (!forward_encapsulation_payload) {
+#if ENABLE_DEBUG 
+            printf("could not add forward encapsulation payload to packet buffer\n");
+#endif 
+            gnrc_pktbuf_release(payload);
+            return;
+        }
+
+        /** build udp header */
+        udp = gnrc_udp_hdr_build(forward_encapsulation_payload, source_port, destination_port);
+    } else {
+        /** build udp header */
+        udp = gnrc_udp_hdr_build(payload, source_port, destination_port);
+    }
+
+    if (!udp) {
+#if ENABLE_DEBUG 
+        printf("could not add udp header to packet buffer\n");
+#endif 
+        gnrc_pktbuf_release(payload);
+        return;
+    }
+
+    /** build ip header */
+    ip = gnrc_ipv6_hdr_build(udp, NULL, &destination);
+    if (!ip) {
+#if ENABLE_DEBUG 
+        printf("could not add ip header to packet buffer\n");
+#endif 
+        gnrc_pktbuf_release(udp);
+        return;
+    }
+
+    /** send packet */
+    if (!gnrc_netapi_dispatch_send(GNRC_NETTYPE_UDP, GNRC_NETREG_DEMUX_CTX_ALL, ip)) {
+#if ENABLE_DEBUG 
+        printf("Error: unable to locate UDP thread\n");
+#endif 
+        gnrc_pktbuf_release(ip);
+        return;
+    }
+}
+
+uint8_t mqttsn_check_wireless_node_id(uint8_t *node_id, uint8_t node_length) {
+    if (node_length > MQTTSN_MAX_WIRELESS_NODE_ID_LENGTH) {
+#if ENABLE_DEBUG 
+        printf("the size of the wireless node identifier is too large. got: %d but maximum length is: %d !\n", node_length, MQTTSN_MAX_WIRELESS_NODE_ID_LENGTH);
+#endif 
+        return -1;
+    }
+
+    if ((!node_id) || (node_length == 0)) {
+        // TODO
+//        snprintf(wireless_node_id, sizeof(wireless_node_id)-1, "%X", mqttsn_get_wireless_node_id(void));
+  //      wireless_node_length = strnlen((char*)packet->wireless_node_id, MQTTSN_MAX_WIRELESS_NODE_ID_LENGTH);
+   // } else {
+//        memcpy(packet->wireless_node_id, wireless_node_id, wireless_node_length);
+    }
+
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ sys/net/application_layer/mqttsn/communication.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2016 Michael Frey <mail@mfrey.net>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     mqtt-sn
+ * @{
+ *
+ * @file        communication.h
+ * @brief       Functions to send and receive MQTT-SN messages
+ *
+ * @author      Michael Frey <mail@mfrey.net>
+ */
+
+#ifndef MQTTSN_COMMUNICATION_H_
+#define MQTTSN_COMMUNICATION_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "net/ipv6/addr.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Initializes the MQTT-SN communication module
+ *
+ * @param[in] src The source host
+ * @param[in] src_port The port at the source host
+ * @param[in] dest The destination host
+ * @param[in] dest_port The port at the destination host
+ * @param[in] enable_forward_encapsulation A status flag which indicates if the
+ * client provides forward encapsulation
+ */
+void mqttsn_communication_init(ipv6_addr_t src, uint16_t src_port, ipv6_addr_t dest, uint16_t dest_port, bool enable_forward_encapsulation);
+
+/**
+ * Provides means to send MQTT-SN messages over UDP.
+ *
+ * @param[in] packet The packet to sent over UDP
+ */
+void mqttsn_communication_send_udp(void *packet);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
