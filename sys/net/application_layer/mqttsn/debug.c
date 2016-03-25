97cdcef6af4964e368fb66f5a09cf41dced0c574
Michael Frey <mail@mfrey.net>
2016-03-25 18:01:21 +0100

    added debug functions for mqtt-sn

---
+++ sys/net/application_layer/mqttsn/debug.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2016 Michael Frey <mail@mfrey.net>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     mqtt-sn
+ * @{
+ *
+ * @file        debug.c
+ * @brief       Helper functions to debug mqtt-sn 
+ *
+ * @author      Michael Frey <mail@mfrey.net>
+ */
+
+#include "debug.h" 
+
+#include "mqttsn.h"
+
+const char* mqttsn_msg_type_to_string(uint8_t msg_type) 
+{
+    switch(msg_type) {
+        case MQTTSN_TYPE_ADVERTISE:       return "ADVERTISE";
+        case MQTTSN_TYPE_SEARCHGW:        return "SEARCHGW";
+        case MQTTSN_TYPE_GWINFO:          return "GWINFO";
+        case MQTTSN_TYPE_CONNECT:         return "CONECT";
+        case MQTTSN_TYPE_CONNACK:         return "CONACK";
+        case MQTTSN_TYPE_WILLTOPICREQ:    return "WILLTOPICREQ";
+        case MQTTSN_TYPE_WILLTOPIC:       return "WILLTOPIC";
+        case MQTTSN_TYPE_WILLMSGREQ:      return "WILLMSGREQ";
+        case MQTTSN_TYPE_WILLMSG:         return "WILLMSG";
+        case MQTTSN_TYPE_REGISTER:        return "REGISTER";
+        case MQTTSN_TYPE_REGACK:          return "REGACK";
+        case MQTTSN_TYPE_PUBLISH:         return "PUBLISH";
+        case MQTTSN_TYPE_PUBACK:          return "PUBACK";
+        case MQTTSN_TYPE_PUBCOMP:         return "PUBCOMP";
+        case MQTTSN_TYPE_PUBREC:          return "PUBREC";
+        case MQTTSN_TYPE_PUBREL:          return "PUBREL";
+        case MQTTSN_TYPE_SUBSCRIBE:       return "SUBSCRIBE";
+        case MQTTSN_TYPE_SUBACK:          return "SUBACK";
+        case MQTTSN_TYPE_UNSUBSCRIBE:     return "UNSUBSCRIBE";
+        case MQTTSN_TYPE_UNSUBACK:        return "UNSUBACK";
+        case MQTTSN_TYPE_PINGREQ:         return "PINGREQ";
+        case MQTTSN_TYPE_PINGRESP:        return "PINGRESP";
+        case MQTTSN_TYPE_DISCONNECT:      return "DISCONNECT";
+        case MQTTSN_TYPE_WILLTOPICUPD:    return "WILLTOPICUPD";
+        case MQTTSN_TYPE_WILLTOPICRESP:   return "WILLTOPICRESP";
+        case MQTTSN_TYPE_WILLMSGUPD:      return "WILMSGUPD";
+        case MQTTSN_TYPE_WILLMSGRESP:     return "WILLMSGRESP";
+        default:                          return "UNKNOWN MESSSAGE TYPE";
+    }
+}
+
---
+++ sys/net/application_layer/mqttsn/debug.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2016 Michael Frey <mail@mfrey.net>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     mqtt-sn
+ * @{
+ *
+ * @file        debug.h
+ * @brief       Helper functions to debug mqtt-sn 
+ *
+ * @author      Michael Frey <mail@mfrey.net>
+ */
+
+#ifndef MQTTSN_DEBUG_H_
+#define MQTTSN_DEBUG_H_
+
+#include "stdint.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Returns a string representation of a given MQTT-SN message type.
+ *
+ * @param[in] msg_type The message type to transform into a string.
+ *
+ * @return Upon success the string representation of the given message type, on
+ * failure a message string containing a error message.
+ */
+const char* mqttsn_msg_type_to_string(uint8_t msg_type);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
